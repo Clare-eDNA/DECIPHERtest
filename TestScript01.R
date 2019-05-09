20fea1d8f342e7d5f2c939d66b8e3cebd8112dd1
Clare Adams <clareadams@anat-imac-31.otago.ac.nz>
2019-05-09 12:53:47 +1200

    Script test and file for Paua Primers

--- TestScript01.R
+++ TestScript01.R
@@ -2,4 +2,99 @@
 #2019 05 09
 #Playing around tihe Decipher in the name of making primers
 
+
 library(DECIPHER)
+?DesignPrimers
+
+setwd("~/Documents/Bioinformatics Scripts & Notes/DECIPHERtest/DECIPHERtest")
+
+# STEP 1
+# READ IN FILES AND GET THEM INTO AN OK FORMAT
+
+#Specify the fasta sequences of the target area
+fas <- "PauaCOXII.fasta"
+
+# specify a path for where to write the sequence database
+
+#Creating the seq database in the memory
+dbConn <- dbConnect(SQLite(), ":memory:")
+N <- Seqs2DB(fas, "FASTA", dbConn, "")
+#Outputs number of total sequences in the table of sequences
+
+N
+#should equal 108 now
+
+#Every sequence here is its own thing
+desc <- as.character(seq_len(N))
+
+#Every sequence here is grouped by a distance matrix and then clustered based on the distance matrix / IDs
+dna <- readDNAStringSet(fas)
+d <- DistanceMatrix(dna, type="dist")
+IdClusters(d, method="complete", cutoff=0.05, showPlot=TRUE, type="dendrogram")
+#Chuck the clusters into an object or something that can be called
+desc<-IdClusters(d, method="complete", cutoff=0.05, showPlot=TRUE, type="dendrogram")
+#Can get at lower branches by specifying which branch you want to look at
+unique(desc[[2]])
+
+#Decided to go with the other ones because apparently everything is its own thing anyways (though probs not true?)
+
+#ID the sequences with a number
+desc <- as.character(seq_len(N))
+#Show the unique descriptors (aka the number of each file)
+unique(desc)
+
+#Add the unique names for the sequences (in this case, their number) to the database as an ID for each seq
+Add2DB(data.frame(identifier=desc), dbConn)
+
+# STEP 2
+# Set up some parameters for designing primers
+
+#> # Designing primers for sequencing experiments:
+#> TYPE <- "sequence"
+#> MIN_SIZE <- 300 # base pairs
+#> MAX_SIZE <- 700
+#> RESOLUTION <- 5 # k-mer signature
+#> LEVELS <- 5 # max number of each k-mer
+
+TYPE <- "sequence"
+MIN_SIZE <-175
+MAX_SIZE <-350
+RESOLUTION <- 5
+LEVELS <- 5
+
+#Now, design primers
+
+primers <- DesignSignatures(dbConn,
+                            type=TYPE,
+                            minProductSize=MIN_SIZE,
+                            maxProductSize=MAX_SIZE,
+                            resolution=RESOLUTION,
+                            levels=LEVELS)
+#It does things now
+
+# STEP 3
+# Look at things
+
+#This code gives out a forward and reverse primer
+primers[which.max(primers$score),]
+
+#PSET to 1 to get the best/first primer
+PSET <- 1
+
+#Some code which does things apparently
+dna <- SearchDB(dbConn,
+                remove="all",
+                nameBy="identifier",
+                clause="row_names =
+                (select min(row_names) from Seqs as S
+                where S.identifier = Seqs.identifier)",
+                verbose=FALSE)
+#code which pulls out the f primer and r primer
+f_primer <- DNAStringSet(primers$forward_primer[PSET])
+r_primer <- DNAStringSet(primers$reverse_primer[PSET])
+
+#something about
+patterns <- c(f_primer,
+              reverseComplement(r_primer))
+BrowseSeqs(dna,
+           patterns=patterns)
